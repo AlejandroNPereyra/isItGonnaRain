914ec1230d835c0430240dbe120dd09c089b0b70
AlejandroNPereyra <pereyra_alejandro@yahoo.com.ar>
2024-03-06 18:19:49 +0100

    + more impro

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -4,16 +4,16 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-  <title>Barcelona Weather Forecast</title>
+  <title>Is It Gonna Rain?</title>
   <style>
     /* Add some basic styling for a weather theme */
     body {
-      font-family: sans-serif; /* Change font family to a generic sans-serif font */
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; /* Change font family to a generic sans-serif font */
       background-image: url('isItGonnaRain.png');
       background-size: cover;
       background-position: center;      
       color: #333; /* Set font color to a dark color for better readability */
-      padding: 20px; /* Add padding to body for better spacing */
+      padding: 10px; /* Add padding to body for better spacing */
     }
     h1 {
       text-align: center;
@@ -21,12 +21,12 @@
     }
     .weather-box {
       border: 1px solid #ddd;
-      padding: 30px;
+      padding: 25px;
       margin: 40px;
       background-color: rgba(255, 255, 255, 0.3); /* Add a semi-transparent white background for better readability */
     }
     .weather-box h3 {
-      text-transform: uppercase;
+      /* text-transform: uppercase; */
       font-weight: bold;
     }
     .outlook-message {
@@ -43,7 +43,7 @@
   </style>
 </head>
 <body>
-  <h1>Barcelona Weather Forecast</h1>
+  <h1>Is It Gonna Rain?</h1>
 
   @if (isset($data['hourly']))
     @foreach ([4, 24, 48] as $timeFrame)
